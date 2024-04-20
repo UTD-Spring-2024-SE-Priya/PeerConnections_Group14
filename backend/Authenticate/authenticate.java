a2a6bd66bd6315e3656fcb3c3bdbbf47f51ff5d3
Dagmawet <125145022+Dagmawet@users.noreply.github.com>
2024-04-20 13:00:20 -0500

    Add files via upload

---
+++ backend/Authenticate/authenticate.java
@@ -0,0 +1,61 @@
+/*
+This code is done using MYSQL, the database url, user name, and password are given
+Database creation, population, and querying are all done in this file to minimize clutter
+The MYSQL jar file needed to run this file is attached as a file in the backend folder 
+*/
+
+
+import java.sql.*;
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+public class authenticate {
+     
+    @SuppressWarnings("resource")
+    public static void main(String[] args) throws FileNotFoundException, SQLException
+    {
+        int count = 0;
+        boolean found = false;
+        Scanner scanIN = new Scanner(System.in);
+        String database = "maybe";
+        String table = "tempy";
+        CreateDatabase.createDB(database);
+        CreateDatabase.createTable(database, table);
+
+        File storedVerfication = new File("test.txt");    //open file 
+        Scanner scan = new Scanner(storedVerfication);
+        String input;
+        String valReadIN[];
+
+        while(scan.hasNextLine())                                  //read in values from file and store in hashmap
+        {
+            input = scan.nextLine();
+            valReadIN= input.split(" ");
+            String l = valReadIN[0].toLowerCase();
+            CreateDatabase.populateTable(database, table , l , valReadIN[1]);
+        }
+        
+
+        while(count < 3 && found == false)
+        {
+            System.out.println("Enter UTD Id: ");                           
+            String UTDId = scanIN.nextLine();
+            UTDId.toLowerCase();
+            System.out.println("Enter password: ");
+            String password = scanIN.nextLine();
+
+            found = CreateDatabase.searchDB(database, table, UTDId, password);
+            if(found == false)
+            {
+                System.out.println("Invalid LogIn attempt " + (count+1));
+                count++;
+            }
+            else
+            {
+                System.out.println("Successful login!");
+            }
+        }
+
+    }
+}
